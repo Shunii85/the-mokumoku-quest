7df04a10bb61a5b10f3375ff565c5e83728245aa
shunii <shunii85@outlook.jp>
2024-09-21 20:34:05 +0900

    take the commit details

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,34 @@
 export async function POST(request: Request) {
   const res = await request.json();
-  console.log(res);
+  const commits = res.commits as any[];
+  const owner = res.repository.owner.name as string;
+  const repo = res.repository.name as string;
+
+  const commitPromises = commits.map((commit) =>
+    fetchCommitDetails(commit.id, owner, repo)
+  );
+
+  try {
+    const commitDetails = (await Promise.all(commitPromises)) as any[];
+    console.table(commitDetails);
+  } catch (error) {
+    console.error(error);
+  }
+
   return Response.json({ res });
+}
+
+async function fetchCommitDetails(
+  commitId: string,
+  owner: string,
+  repo: string
+) {
+  const res = await fetch(
+    `https://api.github.com/repos/${owner}/${repo}/commits/${commitId}`,
+    {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+      },
+    }
+  );
 }
