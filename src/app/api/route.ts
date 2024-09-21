df9a4226065d1356d92dd63cb6c15e5088794a20
shunii <shunii85@outlook.jp>
2024-09-21 21:09:37 +0900

    maybe send discord

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,6 @@
+const WEBHOOK_URL =
+  "https://discord.com/api/webhooks/1287022195998195825/MHpni3NvQox3jN9EUI3sYocAldc7KEC_iUM7N3ohID1k4qqUS_Zi2WwvMVe0OYmLwXP5";
+
 export async function POST(request: Request) {
   const res = await request.json();
   const commits = res.commits as any[];
@@ -8,15 +11,14 @@ export async function POST(request: Request) {
     fetchCommitDetails(commit.id, owner, repo)
   );
 
-  console.log("Fetching commit details...");
-
   try {
     const commitDetails = (await Promise.all(commitPromises)) as any[];
-    console.table(commitDetails);
-    return Response.json(commitDetails);
+    await sendDiscord(WEBHOOK_URL, JSON.stringify(commitDetails));
   } catch (error) {
     console.error(error);
   }
+
+  return new Response("OK", { status: 200 });
 }
 
 async function fetchCommitDetails(
@@ -32,4 +34,22 @@ async function fetchCommitDetails(
       },
     }
   );
+}
+
+async function sendDiscord(url: string | undefined, content: string) {
+  if (!url) throw new Error("Discord webhook URL is not provided");
+
+  const data = {
+    username: "GitHub",
+    avatar_url:
+      "https://play-lh.googleusercontent.com/s6_2YQD5GewqFv7zyvDhTpg9TLl96A-sNy_GVqGS-Ukdkfzc_ZCK2dFtgrzm8e43-g",
+    content,
+  };
+
+  const headers = { "Content-Type": "application/json" };
+  const body = JSON.stringify(data);
+
+  const { ok } = await fetch(url, { method: "POST", headers, body });
+
+  if (!ok) throw new Error("Failed to send message to Discord\n");
 }
